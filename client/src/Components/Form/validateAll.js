2d01eedc5068bc32e3f357f5805e4da789e57e77
pablo herrera <valeherreradelarua@gmail.com>
2023-03-29 02:29:02 -0300

    fixing validate inputs

--- client/src/Components/Form/validateAll.js
+++ client/src/Components/Form/validateAll.js
@@ -2,12 +2,13 @@ import validate from "./validate";
 
 export default function validateAll (dog) {
     let errors = {}
+    console.log("VALIDATE ALL RUNNING")
     errors = {
         ...errors,
         ...validate("name", dog.name),
         ...validate("weightMin", dog.weightMin),
         ...validate("weightMax", dog.weightMax),
-        ...validate("heightMin", dog.heightMax),
+        ...validate("heightMin", dog.heightMin),
         ...validate("heightMax", dog.heightMax),
         ...validate("life", dog.life),
         ...validate("temperament", dog.temperament)
